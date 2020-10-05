018f048536d116b910c902c67af06e89d1c07275
Maria <mdelgado@criptext.com>
2020-10-04 21:22:38 -0500

    mi primera app basica con angular

--- angular-hola-mundo/src/app/app.module.ts
+++ angular-hola-mundo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SaludadorComponent } from './saludador/saludador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaludadorComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-hola-mundo/src/app/saludador/saludador.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SaludadorComponent } from './saludador.component';
+
+describe('SaludadorComponent', () => {
+  let component: SaludadorComponent;
+  let fixture: ComponentFixture<SaludadorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SaludadorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SaludadorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
