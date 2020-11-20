f4cb58c04f93e098afc813fbc3298b245ad11096
Maria <mdelgado@criptext.com>
2020-11-20 16:31:15 -0500

    Manejo de rutas

---
+++ angular-wishlist/src/app/destino-detalle/destino-detalle.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DestinoDetalleComponent } from './destino-detalle.component';
+
+describe('DestinoDetalleComponent', () => {
+  let component: DestinoDetalleComponent;
+  let fixture: ComponentFixture<DestinoDetalleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DestinoDetalleComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DestinoDetalleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
